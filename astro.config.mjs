de36c171a6fa1ddfea0d4eb74b6c39d61e87c219
Luis Pacheco <laps1308@gmail.com>
2025-03-14 20:20:28 -0500

    added ci github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
+import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
-
-    i18n: {
-        locales: ["es", "en"],
-        defaultLocale: "es",
-      }
+  integrations: [tailwind()],
+  i18n: {
+    locales: ["es", "en"],
+    defaultLocale: "es",
+  },
 });
